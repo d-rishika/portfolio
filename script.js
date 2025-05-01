d5939a653506b77ad8653fcd959e8620a3a3456c
d-rishika <161288747+d-rishika@users.noreply.github.com>
2025-05-01 23:07:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+<script>
+        var tablinks = document.getElementsByClassName("tablinks");
+        var tabcontents = document.getElementsByClassName("tabcontents");
 
+        function opentab(tabname){
+           for( links of tablinks){
+            links.classList.remove("activelink");
+           }
+           for( contents of tabcontents){
+            contents.classList.remove("activetab");
+           }
+           event.currentTarget.classList.add("activelink");
+           document.getElementById(tabname).classList.add("activetab")
+        }
+
+
+        var sidemenu = document.getElementById("sidemenu");
+
+        function openmenu(){
+           sidemenu.style.right = "0";
+        }
+        function closemenu(){
+            sidemenu.style.right = "-200px";
+        }
+     </script>
