fe5d327aef044c3e3d79a6df00ea7fb15b5fea17
d-rishika <161288747+d-rishika@users.noreply.github.com>
2025-05-02 16:49:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,24 +2,14 @@
         var tablinks = document.getElementsByClassName("tablinks");
         var tabcontents = document.getElementsByClassName("tabcontents");
 
-        function opentab(tabname){
-           for( links of tablinks){
+        function opentab(tabname,event){
+           for( let links of tablinks){
             links.classList.remove("activelink");
            }
-           for( contents of tabcontents){
+           for( let contents of tabcontents){
             contents.classList.remove("activetab");
            }
            event.currentTarget.classList.add("activelink");
            document.getElementById(tabname).classList.add("activetab")
-        }
-
-
-        var sidemenu = document.getElementById("sidemenu");
-
-        function openmenu(){
-           sidemenu.style.right = "0";
-        }
-        function closemenu(){
-            sidemenu.style.right = "-200px";
         }
      </script>
