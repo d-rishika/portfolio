3efbac74bf270210db9c8700b5b954ea87b3aa23
Rishika <danturirishika@gmail.com>
2025-05-09 15:18:39 +0530

    added changes in style.css

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
         var tablinks = document.getElementsByClassName("tablinks");
         var tabcontents = document.getElementsByClassName("tabcontents");
-
+        
         function opentab(tabname,event){
-           for( let links of tablinks){
+           for( links of tablinks){
             links.classList.remove("activelink");
            }
-           for( let contents of tabcontents){
+           for( contents of tabcontents){
             contents.classList.remove("activetab");
            }
            event.currentTarget.classList.add("activelink");
-           document.getElementById(tabname).classList.add("activetab")
+           document.getElementById(tabname).classList.add("activetab");
         }
